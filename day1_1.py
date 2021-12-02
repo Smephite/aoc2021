5017b0abc31486db4b4c23bd2b6a2d2c303098c2
Kai Berszin <mail@kaibersz.in>
2021-12-02 10:40:18 +0100

    Clean up day1

--- day1_1.py
+++ day1_1.py
@@ -1,19 +1,15 @@
-import sys
+import aoc
 
-fileName = ""
-if len(sys.argv) == 2:
-    fileName = str(sys.argv[1])
-else:
-    fileName = input("Path to puzzle input: ")
+input = aoc.aoc()
 
-with open(fileName) as file:
-    last_value = None
-    larger = 0
-    for line in file.readlines():
-        line = line.replace("\n", "")
-        value = int(line)
-        if last_value != None:
-            if last_value < value:
-                larger += 1
-        last_value = value
-    print(f'We got {larger} values larger than their previous.')
+larger = 0
+last_value = None
+
+for line in input:
+    value = int(line)
+    if last_value != None:
+        if last_value < value:
+            larger += 1
+    last_value = value
+
+print(f'We got {larger} values larger than their previous.')
