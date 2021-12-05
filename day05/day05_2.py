98a6f48fdd6c7cef86e0b3a03aefb9bea048e228
Kai Berszin <mail@kaibersz.in>
2021-12-05 06:37:25 +0100

    Add solution for day5

---
+++ day05/day05_2.py
@@ -0,0 +1,66 @@
+import sys, os
+from typing import List
+sys.path.append(os.path.abspath(".."))
+from aoc21 import aoc
+
+lines : List[str]= aoc.aoc()
+positions = {}
+
+for line in lines:
+    data = line.split(" -> ")
+    start = data[0].split(",")
+    end = data[1].split(",")
+
+    start_x = int(start[0])
+    start_y = int(start[1])
+    end_x = int(end[0])
+    end_y = int(end[1])
+
+    if abs(end_x-start_x) == abs(end_y-start_y): # diagonal
+        for i in range(0, abs(end_x-start_x)+1):
+            
+            if end_x < start_x:
+                x = start_x - i
+            else:
+                x = start_x + i
+            
+            if end_y < start_y:
+                y = start_y - i
+            else:
+                y = start_y + i
+            
+            pos = f"{x},{y}"
+            if pos not in positions.keys():
+                positions[pos] = 0
+            positions[pos] = positions[pos]+1
+        continue
+
+    # order lines to be asc
+    if end_x < start_x:
+        temp = end_x
+        end_x = start_x
+        start_x = temp
+
+    if end_y < start_y:
+        temp = end_y
+        end_y = start_y
+        start_y = temp
+
+
+    if start_x == end_x:
+        for y in range(start_y, end_y + 1):
+            pos = f"{start_x},{y}"
+            if pos not in positions.keys():
+                positions[pos] = 0
+            positions[pos] = positions[pos]+1
+
+    elif start_y == end_y:
+        for x in range(start_x, end_x + 1):
+            pos = f"{x},{start_y}"
+            if pos not in positions.keys():
+                positions[pos] = 0
+            positions[pos] = positions[pos]+1
+
+
+
+print("The amount of points with overlap counter >=2 is:", len(list(filter(lambda x: x>=2, positions.values()))))
