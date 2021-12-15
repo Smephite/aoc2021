c301761ba93eaa1392b782f9796b58f4f943f143
Kai Berszin <mail@kaibersz.in>
2021-12-16 00:06:52 +0100

    Add solution for day15

---
+++ day15/day15_1.py
@@ -0,0 +1,91 @@
+import sys, os
+sys.path.append(os.path.abspath("."))
+import aoc
+import heapq
+
+class Node:
+    def __init__(self, position, risk_level) -> None:
+        self.distance = None
+        self.previous = None
+        self.position = position
+        self.risk_level = risk_level
+        self.visited = False
+
+    def __repr__(self) -> str:
+        return f"Node[{self.position}, {self.risk_level}, {self.distance}, {self.previous.position if self.previous is not None else None}, {self.visited}]"
+
+
+    def __str__(self) -> str:
+        return f"Node[{self.position}, {self.risk_level}, {self.distance}, {self.previous.position if self.previous is not None else None}, {self.visited}]"
+
+    def __lt__(self, other):
+        return self.f < other.f
+    
+    def get_min_distance(self) -> int:
+        return self.distance
+    def get_previous_node(self):
+        return self.previous
+
+input = aoc.aoc()
+
+nodes = {}
+
+for y in range(len(input)):
+    for x in range(len(input[0])):
+        pos = (x, y)
+        node = Node(pos, int(input[y][x]))
+        nodes[pos] = node
+
+nodes[(0, 0)].distance = 0
+
+pos_output = (len(input)-1, len(input[0])-1)
+
+
+to_explore = []
+
+heapq.heapify(to_explore)
+
+to_explore.append((0, (0, 0))) # add starting node
+
+while True:
+
+    current_node = None
+    while current_node == None:
+        current_node = heapq.heappop(to_explore)
+        if current_node == None:
+            break # empty
+        if nodes[current_node[1]].visited:
+            current_node = None
+
+    if current_node == None:
+        print(f"There is no path!")
+        break
+
+    current_node = nodes[current_node[1]]
+
+    if current_node.position == pos_output:
+        break
+
+    current_node.visited = True
+
+    position = current_node.position
+
+    for pos in (position[0], position[1]+1), (position[0], position[1]-1), (position[0]+1, position[1]), (position[0]-1, position[1]):
+        if pos not in nodes:
+            continue
+
+        n = nodes[pos]
+
+        dist = current_node.distance + n.risk_level
+
+        if n.distance != None and n.distance < dist:
+            continue
+        
+        n.previous = current_node
+        n.distance = dist
+        
+        heapq.heappush(to_explore, (dist, n.position))
+
+    
+
+print(f"The lowest total risk is {nodes[pos_output].distance}")
